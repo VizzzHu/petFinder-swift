37d0515941536c5b96f65a85a81bec5de201ef35
Xianlin Hu <woshihu@hotmail.com>
2017-09-15 09:45:47 -0700

    add foundControl

---
+++ PetsFinder2/FoundControl.swift
@@ -0,0 +1,15 @@
+//
+//  FoundControl.swift
+//  PetsFinder2
+//
+//  Created by Xianlin Hu on 9/14/17.
+//  Copyright © 2017 Xianlin Hu. All rights reserved.
+//
+
+import UIKit
+
+class FoundControl: UIStackView {
+
+  // MARK: Initialization
+  
+}
